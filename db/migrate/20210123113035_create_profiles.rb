9d7e08c32021aa32bcd5feee34b389dba9811564
Eric Bawakuno <baikaldataanalytics@gmail.com>
2021-01-28 20:12:58 +0200

    Added avatar column

--- db/migrate/20210123113035_create_profiles.rb
+++ db/migrate/20210123113035_create_profiles.rb
@@ -7,6 +7,7 @@ class CreateProfiles < ActiveRecord::Migration[6.0]
     	t.string :job_title
     	t.string :phone_number
     	t.string :contact_email
+
     	t.text :description
 
     	t.timestamps
